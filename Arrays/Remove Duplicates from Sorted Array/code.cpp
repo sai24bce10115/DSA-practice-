ff53f3e4554acd8478adb5a1a820f5e996e0ddf8
sai24bce10115 <sai.24bce10115@vitbhopal.ac.in>
2026-06-11 00:04:09 +0530

    Update code.cpp

--- Arrays/Remove Duplicates from Sorted Array/code.cpp
+++ Arrays/Remove Duplicates from Sorted Array/code.cpp
@@ -1,1 +1,13 @@
-
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int n=nums.size(),i=0;
+        for(int j=i+1;j<n;++j){
+            if(nums[i]!=nums[j]){
+                ++i;
+                nums[i]=nums[j];
+            }
+        }
+        return i+1;
+    }
+};
